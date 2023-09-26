51964ffd67afae1a8a475c7e528bd79f87563f57
PaulCohen <me@pjcohen.com>
2023-09-26 10:26:22 -0600

    fixed it

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const containerDiv = document.querySelector("#container");
 
 function makeRows(rows, columns) {
-    containerDIV.style.setProperty("--grid-rows", rows);
-    containerDIV.style.setProperty("--grid-columns", columns);
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", columns);
     for (i = 0; i <(rows * columns); i++) {
         let square = document.createElement("div");
-       // cell.innerText = (c + 1);//
-        containerDIV.appendChild(square).className = "grid-item";
+
+        containerDiv.appendChild(square).className = "grid-item";
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "pink";
             console.log(square);
