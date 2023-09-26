e5b2eeef4e84b32fc8fe68706674c5f5fe5ed339
PaulCohen <me@pjcohen.com>
2023-09-26 09:26:33 -0600

    broke it

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const container = document.getElementById("container");
+const containerDiv = document.querySelector("#container");
 
-function makeRows(rows, cols){
-    container .style.setProperty('--grid-rows', rows);
-    container .style.setProperty('--grid-cols', cols);
-    for (c = 0; c <(rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell). className = "grid-item";
-    };
-};
+function makeRows(rows, columns) {
+    containerDIV.style.setProperty("--grid-rows", rows);
+    containerDIV.style.setProperty("--grid-columns", columns);
+    for (i = 0; i <(rows * columns); i++) {
+        let square = document.createElement("div");
+       // cell.innerText = (c + 1);//
+        containerDIV.appendChild(square).className = "grid-item";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "pink";
+            console.log(square);
+        })
+    }
+}
 
 makeRows(16, 16);
