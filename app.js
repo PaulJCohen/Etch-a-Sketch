36344cb1166121eaf34db8d732022f0c77af33d9
PaulCohen <me@pjcohen.com>
2023-09-26 10:36:32 -0600

    redoing it

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const containerDiv = document.querySelector("#container");
 
-function makeRows(rows, columns) {
+function makeGrid(rows, columns) {
+
+    while(document.querySelector("button") !== null){
+        document.querySelector("button").remove();
+    }
+     
+
     containerDiv.style.setProperty("--grid-rows", rows);
     containerDiv.style.setProperty("--grid-columns", columns);
-    for (i = 0; i <(rows * columns); i++) {
+    containerDiv.style.width = "960px";
+    containerDiv.style.overflow = "hidden";
+    for (i = 0; i < (rows * columns); i++) {
         let square = document.createElement("div");
-
+        square.style.minHeight = "0";
+        square.style.minWidth = "0";
+        square.style.overflow = "hidden";
         containerDiv.appendChild(square).className = "grid-item";
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "pink";
-            console.log(square);
+        square.addEventListener("mouseover", () =>{
+            square.style.backgroundColor = "blue";
         })
-    }
-}
 
-makeRows(16, 16);
+    }
+    createButton();
+}
